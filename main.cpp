1c0dd071a8b48b94ff7b4eea7282ff29c7b8e78b
kobayashi masato <k024g1031@g.neec.ac.jp>
2024-10-15 16:57:42 +0900

    タイトル画面作成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "GC1A_03_クサマリョウト_タイトル";
+const char kWindowTitle[] = "このハナの一枝の内には尽きることのない言葉がこもっているから、生半可な気持ちと思わないで。";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -9,8 +9,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	enum Scene {
 		START,
@@ -18,7 +18,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		CLEAR,
 		END
 
-	}scene = PLAY;
+	}scene = START;
+
+#pragma region タイトル用の変数
+
+	//画像
+	int titleBackgroundGraph = Novice::LoadTexture("./Resources/Images/titleBackground.png");
+	int titleLogoGraph = Novice::LoadTexture("./Resources/Images/titleLogo.png");
+
+	//ロゴの画像の切り取り開始位置
+	int titleLogoCutX = 1280 * 3;
+
+	//フェードイン・アウト
+	int titleFadeInOut = 255;
+	int titleFadeTimer = 0;
+
+	//ボタンが押されたか確認する変数
+	bool gameIsStart = false;
+
+	//タイトル画面用のタイマー
+	int titleTimer = 0;
+
+
+#pragma endregion
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -32,15 +55,104 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		switch (scene)
 		{
-		case START:
-			
-			//スペース押したらシーン切り替え
-			if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
+#pragma region タイトル画面
 
-				scene = PLAY;
+		case START:
+
+#pragma region 更新処理
+
+			//タイトル画面用のタイマーを加算
+			titleTimer++;
+
+
+			//ボタンが押されたか
+			if (gameIsStart) {
+				titleFadeTimer++;
+				//タイトルロゴのアニメーション
+				if (titleFadeTimer % 5 == 0) {
+					titleLogoCutX += 1280;
+				}
+				//フェードイン・アウト
+				if (titleFadeInOut < 255) {
+					titleFadeInOut += 5;
+
+				}
+
+				//ボタンが押されてから一定時間後
+				if (titleFadeTimer > 140) {
+					//タイトル用変数のリセット
+					titleLogoCutX = 1280 * 3;
+					titleFadeInOut = 255;
+					titleFadeTimer = 0;
+					gameIsStart = false;
+					titleTimer = 0;
+
+					//ゲーム画面へ
+					scene = PLAY;
+				}
+			} else {
+				//フェードイン・アウト
+				if (titleFadeInOut > 0) {
+					if (titleTimer > 50) {
+						titleFadeInOut -= 5;
+					}
+				}
+				//タイトルロゴのアニメーション
+				if (titleLogoCutX > 0) {
+
+					if (titleTimer > 150 && titleTimer % 5 == 0) {
+						titleLogoCutX -= 1280;
+					}
+				} else {
+					//キーが押されるとスタート
+					for (int i = 0; i < 256; i++) {
+						if (keys[i] && preKeys[i] == 0) {
+							gameIsStart = true;
+						}
+					}
+
+					//ボタンが押されるとスタート
+					if (Novice::IsTriggerButton(0, kPadButton0) || Novice::IsTriggerButton(0, kPadButton1) ||
+						Novice::IsTriggerButton(0, kPadButton2) || Novice::IsTriggerButton(0, kPadButton3) || 
+						Novice::IsTriggerButton(0, kPadButton4) || Novice::IsTriggerButton(0, kPadButton5) || 
+						Novice::IsTriggerButton(0, kPadButton6) || Novice::IsTriggerButton(0, kPadButton7) || 
+						Novice::IsTriggerButton(0, kPadButton8) || Novice::IsTriggerButton(0, kPadButton9) ||
+						Novice::IsTriggerButton(0, kPadButton10) || Novice::IsTriggerButton(0, kPadButton11)) {
+						gameIsStart = true;
+					}
+
+
+				}
 			}
 
+
+
+
+
+#pragma endregion
+
+#pragma region 描画処理
+			//背景
+			Novice::DrawSprite(0, 0, titleBackgroundGraph, 1.0f, 1.0f, 0.0f, WHITE);
+
+			//タイトルロゴ
+			if (titleTimer > 150) {
+				Novice::DrawSpriteRect(0, 0, titleLogoCutX, 0, 1280, 720, titleLogoGraph, 1.0f / 4.0f, 1.0f, 0.0f, WHITE);
+			}
+
+
+			//フェードイン・アウト
+			if (titleFadeInOut > 0) {
+				Novice::DrawBox(0, 0, 1280, 720, 0.0f, 0x00000000 + titleFadeInOut, kFillModeSolid);
+			}
+
+			Novice::ScreenPrintf(0, 30, "%d", gameIsStart);
+
+#pragma endregion
 			break;
+
+#pragma endregion
+
 		case PLAY:
 
 			break;
@@ -51,8 +163,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 		Novice::ScreenPrintf(0, 0, "scene = %d", scene);
-		Novice::ScreenPrintf(40, 40, "fuck");
-		
+
 		/// ↑描画処理ここまで
 		///
 
